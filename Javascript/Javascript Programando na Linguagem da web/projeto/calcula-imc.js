2122ea28e23d971459567f2fa1a299ec74ff743b
cledson.alves <cledsonjr@gmail.com>
2016-04-05 13:43:20 -0300

    Módulo 4

--- Javascript/Javascript Programando na Linguagem da web/projeto/calcula-imc.js
+++ Javascript/Javascript Programando na Linguagem da web/projeto/calcula-imc.js
@@ -1,15 +1,31 @@
-var tdPeso = document.getElementById("peso-2");
+/*var tdPeso = document.getElementById("peso-2");
 var tdAltura = document.getElementById("altura-2");
 
 var paciente = {};
 paciente.peso = tdPeso.textContent;
-paciente.altura = tdAltura.textContent;
+paciente.altura = tdAltura.textContent;*/
 
-if(paciente.altura > 0){
-	var imc = paciente.peso / (paciente.altura * paciente.altura);
-	var tdImc = document.getElementById("imc-2");
-	tdImc.textContent = imc;
-	console.info(imc);
-}else{
-	console.info("Altura não permitida");
+var trsPacientes = document.getElementsByClassName("paciente");
+
+for(var posicaoAtual = 0; posicaoAtual <= trsPacientes.length -1; posicaoAtual++){
+	var pacienteTr = trsPacientes[posicaoAtual];
+	
+	var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+	var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+	var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+	
+	var paciente = {};
+	paciente.nome = tdNome.textContent;
+	paciente.peso = tdPeso.textContent;
+	paciente.altura = tdAltura.textContent;
+	
+	if(paciente.altura > 0){
+		var imc = paciente.peso / (paciente.altura * paciente.altura);
+		var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+		tdImc.textContent = imc;
+		console.info(imc);
+	}else{
+		console.info("Altura não permitida");
+	}
 }
+
