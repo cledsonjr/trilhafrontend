a43bdb3465fb7d38eea0362ab20cfe0022b1b7c0
cledson.alves <cledsonjr@gmail.com>
2016-04-05 13:19:48 -0300

    Módulo 3

--- Javascript/Javascript Programando na Linguagem da web/projeto/calcula-imc.js
+++ Javascript/Javascript Programando na Linguagem da web/projeto/calcula-imc.js
@@ -1,8 +1,14 @@
-var peso = 115;
-var altura = 1.88;
+var tdPeso = document.getElementById("peso-2");
+var tdAltura = document.getElementById("altura-2");
 
-if(altura > 0){
-	var imc = peso / (altura * altura);
+var paciente = {};
+paciente.peso = tdPeso.textContent;
+paciente.altura = tdAltura.textContent;
+
+if(paciente.altura > 0){
+	var imc = paciente.peso / (paciente.altura * paciente.altura);
+	var tdImc = document.getElementById("imc-2");
+	tdImc.textContent = imc;
 	console.info(imc);
 }else{
 	console.info("Altura não permitida");
